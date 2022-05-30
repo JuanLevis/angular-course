d13115b8b58318331c905fd34f702d9f2269b894
JuanLevis <juan.levis9425@gmail.com>
2022-05-30 15:13:13 -0300

    Added alert for error messages

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -19,6 +19,10 @@ export class AuthComponent {
     this.isLoginMode = !this.isLoginMode;
   }
 
+  onHandleError() {
+    this.error = null;
+  }
+
   onSubmit(authForm: NgForm) {
     if (!authForm.valid) {
       return;
